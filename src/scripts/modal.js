60ba24f97ae179df462d89e05ce993b375b2b20d
Sandra <sandra@magdalena>
2016-12-09 14:19:55 +0100

    adjustments to alert dialog

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,12 +1,15 @@
 var alert = $('#alert');
+var $overlay = $('#overlay');
 var vals = ['true', 'false'];
 
 $("#modal-open-button").click(function(){
 	alert.show();
-    alert.attr('aria-hidden', 'false');
+	$overlay.show();
+	alert.attr('aria-hidden', 'false');
 });
 
-$("#modal-close-button").click(function(){
+$("#modal-ok-button").click(function(){
 	alert.hide();
-    alert.attr('aria-hidden', 'true');
+	alert.attr('aria-hidden', 'true');
+	$overlay.hide();
 });

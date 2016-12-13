e7ee7c99a9806003c7f0e5599e3235dd4c632b4e
Sandra <sandra@magdalena>
2016-12-13 10:56:23 +0100

    deleted unneccessary files

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -1,24 +1,24 @@
 $('.accordion-tab').click(function(){
 
-  var $this = $(this);
+	var $this = $(this);
 
-  var vals = ['true', 'false'];
-  toggleAttr($($this.next()), 'aria-hidden', vals);
-  toggleAttr($($this),'aria-selected', vals);
-  toggleAttr($($this),'aria-expanded', vals);
+	var vals = ['true', 'false'];
+	toggleAttr($($this.next()), 'aria-hidden', vals);
+	toggleAttr($($this),'aria-selected', vals);
+	toggleAttr($($this),'aria-expanded', vals);
 
-  $this.next().toggle();
+	$this.next().toggle();
 
 });
 
 function toggleAttr(el, attribute, vals){
 
-  if ($(el).attr(attribute) ===  vals[0]){
-      $(el).attr(attribute, vals[1]);
-  } else if ($(el).attr(attribute) === vals[1])
+	if ($(el).attr(attribute) ===  vals[0]){
+			$(el).attr(attribute, vals[1]);
+	} else if ($(el).attr(attribute) === vals[1])
 
-  {
-    $(el).attr(attribute, vals[0]);
-  }
+	{
+		$(el).attr(attribute, vals[0]);
+	}
 
 };
--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -9,40 +9,43 @@ $('#menuitem2').click(function(){
 	submenu.toggle();
 	content.hide();
 	var vals = ['true', 'false'];
-  toggleAttr($(submenu), 'aria-hidden', vals);
+	toggleAttr($(submenu), 'aria-hidden', vals);
 
 });
 
 $('#content1-menu').click(function(){
 
-		content1.show();
-		content2.hide();
-		content3.hide();
+	content1.show();
+	content2.hide();
+	content3.hide();
+
 });
 
 $('#content2-menu').click(function(){
 
-		content2.show();
-		content1.hide();
-		content3.hide();
+	content2.show();
+	content1.hide();
+	content3.hide();
+
 });
 
 $('#content3-menu').click(function(){
 
-		content3.show();
-		content2.hide();
-		content1.hide();
+	content3.show();
+	content2.hide();
+	content1.hide();
+
 });
 
 
 function toggleAttr(el, attribute, vals){
 
-  if ($(el).attr(attribute) ===  vals[0]){
-      $(el).attr(attribute, vals[1]);
-  } else if ($(el).attr(attribute) === vals[1])
+	if ($(el).attr(attribute) ===  vals[0]){
+		$(el).attr(attribute, vals[1]);
+	} else if ($(el).attr(attribute) === vals[1])
 
-  {
-    $(el).attr(attribute, vals[0]);
-  }
+	{
+		$(el).attr(attribute, vals[0]);
+	}
 
 };
